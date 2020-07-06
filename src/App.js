d2c56597af03e994f6587bb56fb254bf723de130
Lina Liao <lina.liao18@gmail.com>
2020-07-05 22:34:46 -0700

    add useContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
 import Search from './components/search';
 import Result from './components/result';
 import TextInputWithFocusButton from './components/useRefPractice';
+import ComponentC from './components/ComponentC';
+
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
 
-  const [data,setData] = useState('');
-  
-  const dataHandler = (data)=>{
+  const [data, setData] = useState('');
+
+  const dataHandler = (data) => {
     setData(data);
   }
 
   return (
     <>
-    <Search result={dataHandler}/>
-   <Result receivedData={data}/>
+      <Search result={dataHandler} />
+      <Result receivedData={data} />
 
-   <TextInputWithFocusButton/>
+      <TextInputWithFocusButton />
 
+      <UserContext.Provider value={'Vishwas'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
 
     </>
   );
